08c792940e0040486d455ac763253cafb363c4ae
Jakaria030 <gulamjakaria99@gmail.com>
2025-10-05 03:09:50 +0600

    Add our achievments section

--- app/page.js
+++ app/page.js
@@ -23,13 +23,13 @@ const HomePage = () => {
   ];
 
   return (
-    <main className="">
+    <main>
       {/* Course section */}
       <section className="relative">
         <div className="absolute top-0 left-0 size-80 bg-teal-400/10 rounded-full blur-3xl"></div>
         <div className="absolute bottom-0 right-0 size-80 bg-teal-400/5 rounded-full blur-3xl"></div>
 
-        <div className="max-w-7xl mx-auto px-4 py-8 sm:px-8 sm:py-16">
+        <div className="max-w-7xl mx-auto px-4 py-8 sm:px-8 sm:py-16 md:py-28">
           <div className="grid gap-8 md:gap-16 grid-cols-1 lg:grid-cols-2">
             {courses.map((course) => (
               <CourseCard
@@ -40,6 +40,38 @@ const HomePage = () => {
                 price={course.price}
               />
             ))}
+          </div>
+        </div>
+      </section>
+
+      {/* Our achievements section */}
+      <section className="text-white">
+        <div className="max-w-7xl mx-auto px-4 pb-8 sm:px-8 sm:pb-16 md:pb-28 text-center">
+          <h2 className="text-3xl md:text-4xl font-bold mb-2">
+            Our Achievements
+          </h2>
+          <p className="text-gray-300 text-lg md:text-xl mb-8">
+            Students enrolled in our courses have achieved amazing ranks in
+            coding contests.
+          </p>
+
+          <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-8">
+            <div className="bg-slate-800 p-6 rounded-lg shadow hover:shadow-lg transition">
+              <h3 className="text-4xl font-bold text-orange-500 mb-2">4</h3>
+              <p className="text-gray-400">Codeforces Expert</p>
+            </div>
+            <div className="bg-slate-800 p-6 rounded-lg shadow hover:shadow-lg transition">
+              <h3 className="text-4xl font-bold text-yellow-400 mb-2">29</h3>
+              <p className="text-gray-400">Codeforces Specialist</p>
+            </div>
+            <div className="bg-slate-800 p-6 rounded-lg shadow hover:shadow-lg transition">
+              <h3 className="text-4xl font-bold text-teal-400 mb-2">106</h3>
+              <p className="text-gray-400">Codeforces Pupil</p>
+            </div>
+            <div className="bg-slate-800 p-6 rounded-lg shadow hover:shadow-lg transition">
+              <h3 className="text-4xl font-bold text-sky-400 mb-2">15</h3>
+              <p className="text-gray-400">BD Big Tech</p>
+            </div>
           </div>
         </div>
       </section>
